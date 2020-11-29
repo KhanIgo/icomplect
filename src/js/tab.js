3baf2ed2161ef2f2be54a8a872db55b4ddbc5689
Igor Khan <i_m@khanigo.pw>
2020-11-30 09:21:05 +1000

    Сделано переключение табов

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,22 +1,22 @@
 import jquery from 'jquery';
 
 jquery( function($){
+
+    $('.js-tab-content').not('.is-active').css('height', '0px');
+
     $('.js-tab').click( function(e){
         e.preventDefault();
-        var time = 250;
+        var time = 400;
         var $this = $(this);
+        $this.addClass('is-active').siblings('.js-tab').removeClass('is-active');
+
         var target = $this.data('target-tab');
         target = '#'+target;
         var $target = $(target);
         var $siblings = $target.siblings('.js-tab-content');
-        $this.addClass('is-active').siblings('.js-tab').removeClass('is-active');
-
-        
-        $siblings.removeClass('is-active');
-        setTimeout( function(){ 
-            $siblings.css('display', 'none');
-            $target.css('display', 'block').addClass('is-active');;
-        }, time);
+        $siblings.animate({ height: '0px'}, time);
+        var targethHeight = $target.find('>div').outerHeight();
+        $target.animate({ height: targethHeight+'px'}, time);
     } );
 
     
